95349ce75d4e0f9f8b0f93cfa78d06301857b63a
Yoel Zeldes <yoelz@fortscale.com>
2016-09-10 22:51:33 +0300

    fixed production SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://yoel-zeldes.github.io'
+SITEURL = 'https://yoel-zeldes.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
